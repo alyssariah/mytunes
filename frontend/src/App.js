2ce53be55f632f4d5f1b47b50a391a29fb070247
Carolyn Nickell <caro.lovin.it@gmail.com>
2020-04-16 12:15:00 -0700

    react router dom installed on front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 import HeaderComponent from './components/HeaderComponent';
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom';
 import FooterComponent from './components/FooterComponent'
 
 
