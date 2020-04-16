e1ab8e9d317fa664256a02f4098d95bbb671754b
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-04-16 09:19:54 -0700

    added controller

---
+++ backend/controllers/favoriteController.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const Favorite = require('../models/item')
+
+const getAll = (req,res) => {
+    Favorite.find({}).then(favorites => {
+        res.json(favorites)
+    })
+}
+
+const createFavorite = (req,res) => {
+    Favorite.create().then(favorite => {
+        res.json(favorite)
+    })
+}
+
+const deleteFavorite = (req,res) => {
+    Favorite.findByIdAndDelete(req.params.id).then(() => {
+        res.json({
+            "status": "success",
+            "msg": "favorite deleted"
+        })
+    })
+}
+
+module.exports = {
+    getAll,
+    createFavorite,
+    deleteFavorite
+}
