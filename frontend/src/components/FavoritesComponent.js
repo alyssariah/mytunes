8f1a8a851629558bd1d8a2bfff39ee523ba46824
Stanley Pascua <stanleypascua@Stanleys-MacBook-Pro.local>
2020-04-16 12:20:09 -0700

    created a favorites component

---
+++ frontend/src/components/FavoritesComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Favorites() {
+    return (
+        <>
+            <div className="favoriteTracks">
+                <h2>Favorites</h2>
+            </div>
+        </>
+    )
+}
+
+export default Favorites
