7b17969d9617f0ffeeecec541e2a85d174a12965
Alyssa Jackson <alyssajackson@rescomp-19-388097.stanford.edu>
2020-04-16 12:19:04 -0700

    api-helper

--- frontend/src/services/api-helper.js
+++ frontend/src/services/api-helper.js
@@ -1,5 +1,20 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "https://alyssa-favorite-music.herokuapp.com/favorites"
+    baseURL: "https://alyssa-favorite-music.herokuapp.com"
   });
+
+  export const getAllFavorites = async () => {
+    const resp = await api.get("/favorites");
+    return resp.data;
+  };
+  
+  export const deleteFavorite = async id => {
+    const resp = await api.delete(`/favorites/${id}`);
+    return resp.data;
+  };
+  
+  export const createFavorite = async track => {
+    const resp = await api.post("/favorites", track);
+    return resp.data;
+  };
