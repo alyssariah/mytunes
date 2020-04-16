f84f3d0d1834c90cc5e9a5ecb020354da5776354
Alyssa Jackson <alyssajackson@rescomp-19-388097.stanford.edu>
2020-04-16 08:33:38 -0700

    index

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
+const parser = require('body-parser')
+const cors = require('cors')
 
+app.use(parser.json())
+app.use(cors())
+
+const favoriteRouter = require('.routes/favorites')
+
+app.use('/favorites', favoriteRouter)
 
 app.listen(PORT, ()=> {"I am conntected on port: ", PORT})
