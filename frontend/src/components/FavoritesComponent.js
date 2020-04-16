949790758874f6b66a67451d64c3cb51ed05ba30
Alyssa Jackson <alyssajackson@rescomp-19-388097.stanford.edu>
2020-04-16 12:33:07 -0700

    add favorites

--- frontend/src/components/FavoritesComponent.js
+++ frontend/src/components/FavoritesComponent.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {getAllFavorites, deleteFavorite, createFavorite} from "../services/api-helper"
 
 function Favorites() {
+    const [favorites, setFavorites] = useState([])
+    const [isLoading, setIsloading] = useState(true);
+    
+    useEffect(() => {
+        const makeAPICall = async () => {
+            const resp = await getAllFavorites();
+            // console.log('useEffect - resp', resp)
+            setUsers(resp);
+            setIsloading(false);
+        };
+        makeAPICall();
+    }, []);
+
+    const renderFavorites= favorites.map((track,index)=> {
+        return(
+          <div className="musicTrack" key={index}>
+            <h3>{track.Song} <i class="far fa-star"></i></h3> 
+            <p>Artist: {track.Artist}</p>
+            <p>Album: {track.Album}</p>
+            <a href={track.previewURL}>Listen to preview</a>
+          </div>
+        )
+      })
     return (
         <>
             <div className="favoriteTracks">
                 <h2>Favorites</h2>
+                {!isLoading && renderFavorites}
             </div>
         </>
     )
